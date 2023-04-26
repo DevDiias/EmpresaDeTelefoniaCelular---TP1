47eb45bdce9895e8e7e8064da9071c4b3f886918
DevDiias <thaiisdiias15@gmail.com>
2023-04-26 15:13:39 -0300

    Primeira alteração

---
+++ src/Recarga.java
@@ -0,0 +1,23 @@
+
+public class Recarga {
+	
+	private Date data;//Verificar se iremos criar uma enum do tipo Date
+	private float valor;
+	
+	public Recarga(Date data, float valor) {
+		
+		
+	}
+	
+	public Data getData() {
+		
+	}
+	public float getValor() {
+		
+	}
+	public String toString() {
+		
+	}
+	
+	
+}
---
+++ src/Telefonia.java
@@ -0,0 +1,48 @@
+
+public class Telefonia {
+	
+	private int numPrePagos;
+	private int numPosPagos;
+	
+	//pendente criar mais 2 atributos:
+	//Vetor de referencias prePago;
+	//Vetor de referencias posPago;
+
+	//---------------------------------------------------------------
+	
+	public void telefonia(){
+		//Metodo construtor - preciso entender o que ele instancia.
+	}
+	
+	public void cadastrarAssinante() {
+		
+	}
+	
+	public void listarAssinante() {
+		
+	}
+	
+	public void fazerChamada() {
+		
+	}
+	
+	public void fazerRecarga() {
+		
+	}
+	
+	private Prepago localizarPrePago(long cpf) {
+		//Necessário criar a classe Prepago para funcionar
+	}
+	
+	private Pospago localizarPosPago(long cpf) {
+		//Necessário criar a classe Pospago para funcionar
+	}
+	
+	public void imprimirFaturas() {
+		
+	}
+	
+	public static void main (String[] args) {
+		
+	}
+}
